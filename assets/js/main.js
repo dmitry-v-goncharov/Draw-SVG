9d7715896a6762941df5d68ef8f1633835dcedb2
dmitry-v-goncharov <dmitry.v.goncharov@gmail.com>
2019-05-04 01:40:11 +0300

    output path

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,17 @@
-const rootEl = document.querySelector('#root');
-
-const wrapperEl = document.createElement('div');
-wrapperEl.classList.add('wrapper');
-rootEl.appendChild(wrapperEl);
-
-const askSection = document.createElement('div');
-askSection.classList.add('viewbox-wrapper');
-wrapperEl.appendChild(askSection);
-
-const viewBoxText = document.createElement('p');
-viewBoxText.textContent = 'Enter viewBox value "X0 Y0 X1 Y1" and click Apply.';
-askSection.appendChild(viewBoxText);
+const headInput   = document.querySelector('#header__input'),
+      headOutput  = document.querySelector('#header__output'),
+      rootEl      = document.querySelector('#root');
 
 const viewBoxInput = document.createElement('input');
 viewBoxInput.type = 'text';
 viewBoxInput.classList.add('viewbox-input');
 viewBoxInput.setAttribute('id', 'viewbox-input');
 viewBoxInput.value = '0 0 512 512';
-askSection.appendChild(viewBoxInput);
+headInput.appendChild(viewBoxInput);
 
-let ns = 'http://www.w3.org/2000/svg';
-let svg = document.createElementNS(ns, 'svg');
-let pathOne = document.createElementNS(ns,'path');
+const ns = 'http://www.w3.org/2000/svg';
+const svg = document.createElementNS(ns, 'svg');
+const pathOne = document.createElementNS(ns,'path');
 
 let svgCreator = {
   svg: function(el, id, w, h, v) {
@@ -52,14 +42,18 @@ let svgCreator = {
   },
   draw: function(args) {
     this.path(pathOne, svg, 'path-one', `M 0 0 ${this.points.join(' ')}`);
+    headOutput.textContent =
+      document.querySelector('#path-one').getAttribute('d');
   }
 };
 
 const viewBoxBtn = document.createElement('button');
 viewBoxBtn.classList.add('viewbox-btn');
 viewBoxBtn.textContent = 'Apply';
-askSection.appendChild(viewBoxBtn);
-viewBoxBtn.addEventListener('click', function(event) {
+headInput.appendChild(viewBoxBtn);
+
+
+viewBoxBtn.addEventListener('click', (event) => {
   let inputValue = viewBoxInput.value;
   let arr = inputValue.split(' ');
 
@@ -67,11 +61,8 @@ viewBoxBtn.addEventListener('click', function(event) {
   svgCreator.width = arr[2];
   svgCreator.height = arr[3];
 
-  svgCreator.svg(svg, 'svg-root', svgCreator.width, svgCreator.height, svgCreator.viewBox);
-  wrapperEl.appendChild(svg);
+  svgCreator.svg(svg, 'svg-root', svgCreator.width,
+                 svgCreator.height, svgCreator.viewBox);
+  rootEl.appendChild(svg);
   svgCreator.addPoints();
-
-  viewBoxInput.remove();
-  viewBoxText.remove();
-  viewBoxBtn.remove();
 });
